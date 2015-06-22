124631bf50725fce1d7ab8e48627b2dd1499bd19
Michael Colenso <github@michaelcolenso.com>
2015-06-22 00:44:46 -0700

    initial commit

---
+++ bp_content/themes/default/config/production.py
@@ -0,0 +1,10 @@
+config = {
+
+    # This config file will be detected in production environment and values defined here will overwrite those in config.py
+    'environment': "production",
+
+    # ----> ADD MORE CONFIGURATION OPTIONS HERE <----
+    'enable_federated_login': False
+
+
+}
---
+++ bp_content/themes/mike/config/production.py
@@ -0,0 +1,14 @@
+config = {
+
+    # This config file will be detected in production environment and values defined here will overwrite those in config.py
+    'environment': "production",
+
+    # ----> ADD MORE CONFIGURATION OPTIONS HERE <----
+    'enable_federated_login': False,
+
+    # contact page email settings
+    'contact_sender': "michaelcolenso77",
+    'contact_recipient': "admin@michaelcolenso.com"
+
+
+}
